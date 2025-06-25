86c3e2d06b3e08e8536bcdc030edff3e2d9f5c01
Mateuzkl <mateus.roberto@educacao.mg.gov.br>
2025-06-25 12:25:29 -0300

    Update config.lua settings

--- config.lua
+++ config.lua
@@ -86,8 +86,8 @@ maxMarketOffersAtATimePerPlayer = 100
 -- MySQL
 mysqlHost = "127.0.0.1"
 mysqlUser = "root"
-mysqlPass = "ag4478985"
-mysqlDatabase = "otg_custom"
+mysqlPass = ""
+mysqlDatabase = ""
 mysqlPort = 3306
 mysqlSock = ""
 passwordType = "sha1"
